9c63688beee3bbab59edd74d14a3ffe72a63cc3b
Eylon <alex.bettini97@gmail.com>
2023-03-25 11:59:07 +0100

    Update get_next_line.c

--- Libft/get_next_line/get_next_line.c
+++ Libft/get_next_line/get_next_line.c
@@ -79,8 +79,9 @@ static char	*ft_rmline(char *buf_old)
 	char	*buf_new;
 
 	i = 0;
-	j = 0;
 	while (buf_old[i] != '\n' && buf_old[i] != '\0')
+		i++;
+	if (buf_old[i] == '\n')
 		i++;
 	if (!buf_old[i])
 	{
@@ -88,11 +89,10 @@ static char	*ft_rmline(char *buf_old)
 		return (NULL);
 	}
 	buf_new = ft_calloc(ft_strlen(buf_old) - i + 1, sizeof(char));
-	i++;
+	j = 0;
 	while (buf_old[i] != '\0')
 	{
-		buf_new[j] = buf_old[i];
-		i++;
+		buf_new[j] = buf_old[i + j];
 		j++;
 	}
 	free(buf_old);
